66207beca22e7a342c30c7424e767bd429ab1977
sooqua <sooqua751@gmail.com>
2017-06-06 15:39:45 +0300

    Fix bug related to using Math.min, add auto zoom feature, minor optimizations.

--- TwitchVODEnhancer.user.js
+++ TwitchVODEnhancer.user.js
@@ -3,7 +3,7 @@
 // @author       sooqua
 // @namespace    https://github.com/sooqua/
 // @downloadURL  https://github.com/sooqua/TwitchVODEnhancer/raw/master/TwitchVODEnhancer.user.js
-// @version      0.2
+// @version      0.3
 // @match        *://*.twitch.tv/*
 // @run-at       document-start
 // @grant        GM_addStyle
@@ -11,13 +11,14 @@
 (function() {
     'use strict';
 
-    var client_id = 'ENTER_YOUR_CLIENT_ID';
-    const canvas_width = 2500;
-    const canvas_height = 50;
-    const slider_height = 2.6;
-    const slider_height_unit = 'em';
-    const zoom = 3;
-    const step = 60000; // msec.
+    const client_id = 'ENTER_YOUR_CLIENT_ID',
+        canvas_width = 2500,
+        canvas_height = 1,
+        slider_height = 2.6,
+        slider_height_unit = 'em',
+        step = 60000, // msec.
+        auto_zoom = 1; // width of one step (%), non-zero values override the 'zoom' value
+    let zoom = 3;
     const gradient = [
         [
             0,
@@ -50,8 +51,8 @@
     ];
     const slider_half_height = slider_height / 2;
 
-    let steps_data_mc = [];
-    let steps_data_ts = [];
+    let steps_data_mc = [],
+        steps_data_ts = [];
 
     let observer;
 
@@ -76,6 +77,68 @@
         if (!slider_handle) return;
         observer.disconnect();
 
+        let vid_id = /twitch.tv\/videos\/(\d+)/.exec(window.location.href)[1];
+
+        let r = await getJson('https://api.twitch.tv/kraken/videos/' + vid_id + '?client_id=' + client_id),
+            vid_start = new Date(r.recorded_at).getTime(),
+            vid_length = r.length * 1000,
+            vid_end = vid_start + vid_length,
+            step_width = Math.round(step / vid_length * canvas_width);
+
+        if (auto_zoom) {
+            zoom = (auto_zoom / (step_width / canvas_width * 100)).clamp(1, 100);
+        }
+
+        GM_addStyle(`
+        .player-seek {
+            top: 0px !important;
+        }
+        .canvasWrapper {
+            transform: translateZ(0) !important;
+            overflow: hidden !important;
+        }
+        .js-player-slider:before {
+            display: none !important;
+        }
+        .js-player-slider > .ui-slider-range {
+            pointer-events: none !important;
+            z-index: 1 !important;
+            background: rgba(169, 145, 212, .5) !important;
+            height: ${slider_height + slider_height_unit} !important;
+            top: 0px !important;
+            transition: initial !important;
+        }
+        .js-player-slider > .ui-slider-handle {
+            pointer-events: none !important;
+            width: .1em !important;
+            height: ${slider_height + slider_height_unit} !important;
+            background: black !important;
+            border: .1em dotted white !important;
+            margin-left: 0em !important;
+            top: 0em !important;
+            border-radius: initial !important;
+            transition: initial !important;
+        }
+        .player-slider--roundhandle .ui-slider-handle:before {
+            display: none !important;
+        }
+        .player-slider__popup-container {
+            box-shadow: none !important;
+            background: hsla(0,0%,0%,.5) !important;
+        }
+        .player-slider__muted-segments {
+            pointer-events: none !important;
+            height: ${slider_half_height + slider_height_unit} !important;
+            top: ${slider_half_height + slider_height_unit} !important;
+        }
+        .player-slider__muted {
+            pointer-events: none !important;
+            height: ${slider_half_height + slider_height_unit} !important;
+        }
+        .sliderCanvas:hover {
+            transform: scale(${zoom}, 1) !important;
+        }`);
+
         let wrapper = document.createElement('div');
         wrapper.className = 'canvasWrapper';
 
@@ -91,74 +154,70 @@
 
         let sheet;
         c.addEventListener('mousemove', function(e) {
-            let r = c.getBoundingClientRect();
-            let p = (e.pageX - r.left) / r.width * 100;
-            c.style.transformOrigin = p + '% center 0px';
-            let scaled_p = (parseFloat(slider_handle.style.left, 10) * zoom - p * zoom + p).clamp(0, 100);
-            sheet = setStyle(`
+            let r = c.getBoundingClientRect(),
+                m = (e.pageX - r.left) / r.width * 100;
+            c.style.transformOrigin = m + '% center 0px';
+            let m_h = (parseFloat(slider_handle.style.left) * zoom - m * zoom + m).clamp(0, 100);
+
+            let s = `
             .ui-slider-handle {
-                left: ${scaled_p}% !important;
+                left: ${m_h}% !important;
             }
             .ui-slider-range {
-                width: ${scaled_p}% !important;
-            }`, sheet);
+                width: ${m_h}% !important;
+            }`;
+
+            let muted_bars = document.querySelectorAll('.player-slider__muted');
+            for (let i = 0, l = muted_bars.length; i < l; ++i) {
+                let m_b = (parseFloat(muted_bars[i].style.left) * zoom - m * zoom + m).clamp(0, 100);
+                s += `
+                .js-muted-segments-container > span:nth-child(${i + 1}) {
+                    left: ${m_b}% !important;
+                    transform: scale(${zoom}, 1) !important;
+                    transform-origin: left !important;
+                }`;
+            }
+
+            sheet = setStyle(s, sheet);
         });
         c.addEventListener('mouseout', function() {
             sheet = setStyle('', sheet);
         });
 
-        let vid_id = /twitch.tv\/videos\/(\d+)/.exec(window.location.href)[1];
-
-        let r = await getJson('https://api.twitch.tv/kraken/videos/' + vid_id + '?client_id=' + client_id);
-
-        let vid_start = new Date(r.recorded_at).getTime();
-        let vid_length = r.length * 1000;
-        let vid_end = vid_start + vid_length;
-
-        let last_ts = vid_start;
-        let last_step_ts = vid_start;
-        let step_msg_count = 0;
-
-        let ctx = c.getContext('2d');
+        let last_step_ts = vid_start,
+            curr_step_mc = 0,
+            ctx = c.getContext('2d');
         ctx.fillStyle = 'rgba(0, 0, 0, .5)';
         ctx.fillRect(0, 0, canvas_width, canvas_height);
-        do {
-            r = await getJson('https://rechat.twitch.tv/rechat-messages?video_id=v' + vid_id + '&start=' + Math.round(last_ts / 1000));
+        for (let ts = vid_start; ts < vid_end; ts += 30000) {
+            r = await getJson('https://rechat.twitch.tv/rechat-messages?video_id=v' + vid_id + '&start=' + Math.round(ts / 1000));
             if (r.data.length === 0) {
-                last_ts = last_ts + 1000;
                 continue;
             }
 
             for (let i = 0; i < r.data.length; i++) {
-                step_msg_count++;
-                let msg_ts = r.data[i].attributes.timestamp;
-                if (msg_ts - last_step_ts >= step) {
-                    steps_data_ts.push(msg_ts);
-                    steps_data_mc.push(step_msg_count);
-                    step_msg_count = 0;
+                curr_step_mc++;
+                let curr_msg_ts = r.data[i].attributes.timestamp;
+                if (curr_msg_ts - last_step_ts >= step) {
+                    steps_data_ts.push(curr_msg_ts);
+                    steps_data_mc.push(curr_step_mc);
+                    curr_step_mc = 0;
 
-                    let steps_data_min = Math.min(...steps_data_mc);
-                    let steps_data_max = Math.max(...steps_data_mc);
+                    let steps_data_mc_max = Math.max(...steps_data_mc);
+                    if (steps_data_mc_max <= 0) continue;
 
-                    for (let i = 0; i < steps_data_mc.length; i++) {
-                        let pos = ((steps_data_ts[i] - vid_start) / (vid_end - vid_start)).clamp(0, 1);
-                        let intensity = ((((steps_data_mc[i] - steps_data_min) / (steps_data_max - steps_data_min)) || 1) * 100).clamp(1, 100);
-                        let color = pickGradientColor(intensity, gradient);
-                        ctx.fillStyle = 'rgb('+color.join()+')';
-                        let w = Math.round(step/vid_length*canvas_width);
-                        ctx.fillRect(Math.round(pos*canvas_width) - w, 0, w, canvas_height);
+                    for (let i = 0, l = steps_data_mc.length; i < l; ++i) {
+                        let pos = ((steps_data_ts[i] - vid_start) / (vid_end - vid_start)).clamp(0, 1),
+                            int = (steps_data_mc[i] / steps_data_mc_max * 100).clamp(1, 100),
+                            col = pickGradientColor(int, gradient);
+                        ctx.fillStyle = 'rgb(' + col.join() + ')';
+                        ctx.fillRect(Math.round(pos * canvas_width) - step_width, 0, step_width, canvas_height);
                     }
 
-                    last_step_ts = msg_ts;
-                }
-
-                if (i === r.data.length - 1) {
-                    if (msg_ts === last_ts)
-                        msg_ts = msg_ts + 1000;
-                    last_ts = msg_ts;
+                    last_step_ts = curr_msg_ts;
                 }
             }
-        } while (last_ts < vid_end);
+        }
     }
     
     function getJson(url) {
@@ -180,22 +239,22 @@
         }
 
         //Get the two closest colors
-        let first_color = gradient[color_range[0]][1];
-        let second_color = gradient[color_range[1]][1];
+        let first_color = gradient[color_range[0]][1],
+            second_color = gradient[color_range[1]][1];
 
         //Calculate ratio between the two closest colors
-        let first_color_x = gradient[color_range[0]][0]/100;
-        let second_color_x = gradient[color_range[1]][0]/100-first_color_x;
-        let slider_x = position/100-first_color_x;
-        let ratio = slider_x/second_color_x;
+        let first_color_x = gradient[color_range[0]][0]/100,
+            second_color_x = gradient[color_range[1]][0]/100-first_color_x,
+            slider_x = position/100-first_color_x,
+            ratio = slider_x/second_color_x;
 
         return pickHex( second_color,first_color, ratio );
     }
 
     function pickHex(color1, color2, weight) {
-        let w = weight * 2 - 1;
-        let w1 = (w+1) / 2;
-        let w2 = 1 - w1;
+        let w = weight * 2 - 1,
+            w1 = (w+1) / 2,
+            w2 = 1 - w1;
         return [Math.round(color1[0] * w1 + color2[0] * w2),
             Math.round(color1[1] * w1 + color2[1] * w2),
             Math.round(color1[2] * w1 + color2[2] * w2)];
@@ -228,56 +287,6 @@
     Number.prototype.clamp = function(min, max) {
         return Math.min(Math.max(this, min), max);
     };
-
-    GM_addStyle(`
-    .player-seek {
-        top: 0px !important;
-    }
-    .canvasWrapper {
-        transform: translateZ(0) !important;
-        overflow: hidden !important;
-    }
-    .js-player-slider:before {
-        display: none !important;
-    }
-    .js-player-slider > .ui-slider-range {
-        pointer-events: none !important;
-        z-index: 1 !important;
-        background: rgba(169, 145, 212, .5) !important;
-        height: ${slider_height + slider_height_unit} !important;
-        top: 0px !important;
-        transition: initial !important;
-    }
-    .js-player-slider > .ui-slider-handle {
-        pointer-events: none !important;
-        width: .1em !important;
-        height: ${slider_height + slider_height_unit} !important;
-        background: black !important;
-        border: .1em dotted white !important;
-        margin-left: 0em !important;
-        top: 0em !important;
-        border-radius: initial !important;
-        transition: initial !important;
-    }
-    .player-slider--roundhandle .ui-slider-handle:before {
-        display: none !important;
-    }
-    .player-slider__popup-container {
-        box-shadow: none !important;
-        background: hsla(0,0%,0%,.5) !important;
-    }
-    .player-slider__muted-segments {
-        pointer-events: none !important;
-        height: ${slider_half_height + slider_height_unit} !important;
-        top: ${slider_half_height + slider_height_unit} !important;
-    }
-    .player-slider__muted {
-        pointer-events: none !important;
-        height: ${slider_half_height + slider_height_unit} !important;
-    }
-    .sliderCanvas:hover {
-        transform: scale(${zoom}, 1) !important;
-    }`);
 
     document.addEventListener('DOMContentLoaded', init);
 })();
